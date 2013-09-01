89706a2936c41d3a601b5c78ca98213210ba6302
Asim Ihsan <angry.blade5619@ihsan.io>
2013-09-01 10:06:52 -0500

    router - routes work
    Important because this is how Backbone controls app flow.

--- frontend/app/scripts/application.js
+++ frontend/app/scripts/application.js
@@ -7,32 +7,17 @@ define([
     'templates',
     'communicator',
     'routes/router',
-    'collections/schedules',
-    'views/schedules',
-], function ($, _, Backbone, JST, Communicator, Router, 
-             SchedulesCollection, SchedulesView) {
+], function ($, _, Backbone, JST, Communicator, Router) {
     'use strict';
+
+    var router = new Router();
+    router.navigate("", {trigger: true});
 
     var template = JST['app/scripts/templates/app.ejs'];
     var App = new Backbone.Marionette.Application();
     App.addRegions({});
     App.addInitializer(function() {
-        $("#app").append(template());
         Backbone.history.start({pushState: true});
-
-        var router = new Router();
-        router.navigate("", {trigger: true});
-
-        this.schedulesCollection = new SchedulesCollection();
-        this.schedulesView = new SchedulesView({
-            collection: this.schedulesCollection
-        });
-        this.schedulesCollection.reset([
-            {slug: 'winter-2014', title: 'Winter 2014'},
-            {slug: 'spring-2015', title: 'Spring 2015'}
-        ]);
-        this.schedulesView.render();
-        $("#app").append(this.schedulesView.el);
     });
 
     return App;
--- frontend/app/scripts/routes/controller.js
+++ frontend/app/scripts/routes/controller.js
@@ -2,7 +2,9 @@
 
 define([
     'backbone.marionette',
-], function(Backbone) {
+    'collections/schedules',
+    'views/schedules',
+], function(Backbone, SchedulesCollection, SchedulesView) {
     'use strict';
 
     var RouteController = Backbone.Marionette.Controller.extend({
@@ -11,9 +13,21 @@ define([
         },
         view_schedule: function() {
             console.log("RouterController schedule");
+            $("#app").empty();
         },
         view_schedules: function() {
             console.log("RouterController schedules");
+            this.schedulesCollection = new SchedulesCollection();
+            this.schedulesView = new SchedulesView({
+                collection: this.schedulesCollection
+            });
+            this.schedulesCollection.reset([
+                {slug: 'winter-2014', title: 'Winter 2014'},
+                {slug: 'spring-2015', title: 'Spring 2015'}
+            ]);
+            this.schedulesView.render();
+            $("#app").empty();
+            $("#app").append(this.schedulesView.el);
         },
     });
 
