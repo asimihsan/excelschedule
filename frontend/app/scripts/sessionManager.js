e25d2cdccd0c073036cd35d4f8e15aa540ed32b9
Asim Ihsan <asim.ihsan@gmail.com>
2013-09-12 21:42:05 +0100

    CSRF - disable CSRF protection
    Debugging failing to set CSRF cookies on the production server.

--- frontend/app/scripts/sessionManager.js
+++ frontend/app/scripts/sessionManager.js
@@ -9,19 +9,10 @@ function($, Backbone, Communicator) {
     var SessionManager = Backbone.Marionette.Controller.extend({
         initialize: function(options) {
             console.log("initialize a Session Manager");
-            this.refreshCsrfToken();
 
             /* event API */
-            Communicator.reqres.setHandler("SessionManager:getCsrfToken",
-                                           this.getCsrfToken, this);
             Communicator.reqres.setHandler("SessionManager:isUserAuthenticated",
                                            this.isUserAuthenticated, this);
-        },
-        refreshCsrfToken: function() {
-            this.csrf_token = $.cookie('csrftoken');
-        },
-        getCsrfToken: function() {
-            return this.csrf_token;
         },
         isUserAuthenticated: function() {
             var promise = $.ajax({
